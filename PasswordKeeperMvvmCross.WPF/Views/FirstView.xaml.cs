7098bd88d5e6428bf40c08de5b4bf43a46efbe72
Alexander Ivichev <ivichev@gmail.com>
2019-08-21 19:21:35 +0300

    Add project files.

---
+++ PasswordKeeperMvvmCross.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace PasswordKeeperMvvmCross.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
